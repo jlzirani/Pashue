cd28e8e7b7cc36adbfd58e1e3260d5bae66199fe
jlzirani <jlzirani@gmail.com>
2018-05-02 23:09:30 +0200

    New class named restApi because ApiObject is not well named

--- backapi.py
+++ backapi.py
@@ -1,5 +1,35 @@
-import requests
-import json
+import requests, json
+
+class restApi:
+  def __init__(self, ip, user):
+    self.ip = ip
+    self.user = user
+
+  def buildBaseUrl(self, path):
+    url = "http://" + self.ip + "/api"
+    if self.user and self.user != "":
+      url += "/" + self.user
+    if path and path != "":
+      url += "/" + path
+    return url
+
+  def get(self, path):
+    r = requests.get( self.buildBaseUrl(path) )
+    return r.json()
+
+  def put(self, path, data):
+    r = requests.put( self.buildBaseUrl(path), json.dumps(data))
+    return r.json()
+
+  def post(self, path, data):
+    r = requests.post( self.buildBaseUrl(path), json.dumps(data))
+    return r.json()
+
+  def delete(self, path):
+    r = requests.delete( self.buildBaseUrl(path) )
+    return r.json()
+ 
+
 
 class ApiObject:
   def __init__(self, config, path):
@@ -12,8 +42,6 @@ class ApiObject:
   def buildBaseUrl(self):
     url = "http://" + self.ip + "/api"
 
-    if self.user and self.user != "":
-      url += "/" + self.user
     if self.path and self.path != "":
       url += "/" + self.path
 
